1dc31f55a4d83f3f231a07c82828ea383ecb66a6
RomanBarbarossa <roman.barbarossa@mail.ru>
2022-12-14 20:39:00 +0300

    Create Random Devision

---
+++ ExampleRandom/Program.cs
@@ -0,0 +1,6 @@
+﻿double numberA = new Random().Next(1, 100);
+Console.WriteLine(numberA);
+double numberB = new Random().Next(1, 100);
+Console.WriteLine(numberB);
+double result = numberA / numberB;
+Console.WriteLine(result);
